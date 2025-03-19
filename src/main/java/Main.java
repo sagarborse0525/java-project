141506dc0456497aeeff08c82d77fa24f7009fb7
Sagar Borse <sagarborse0525@gmail.com>
2025-03-19 19:13:40 +0000

    added

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        MessageUtil messageUtil = new MessageUtil("Hello World!");
+        messageUtil.printMessage();
+        messageUtil.salutationMessage();
+    }
+}
+
